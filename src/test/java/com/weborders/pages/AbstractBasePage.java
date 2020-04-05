bce8584b4c0c8cb90f985b427310b06ad62cbb9d
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-05 16:26:00 -0400

    page classes

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -1,8 +1,12 @@
 package com.weborders.pages;
 
 import com.weborders.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -12,7 +16,19 @@ public abstract class AbstractBasePage {
     protected WebDriver driver = Driver.getDriver();
     protected WebDriverWait wait = new WebDriverWait(driver, 20);
 
+    @FindBy(tagName = "h1")
+    protected WebElement pageLogo;
+
+    public String getPageLogoText() {
+        return pageLogo.getText();
+    }
+
     public AbstractBasePage() {
         PageFactory.initElements(driver, this);
+    }
+
+    public void navigateTo(String component) {
+        String locator = "//a[text()='" + component + "']";
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
     }
 }
