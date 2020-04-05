e6c48178e45ce19678a46d5e3594ba8437dc6109
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-05 16:46:10 -0400

    page classes

--- src/test/java/com/weborders/pages/LoginPage.java
+++ src/test/java/com/weborders/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package com.weborders.pages;
 
+import com.weborders.utilities.BrowserUtilities;
 import com.weborders.utilities.ConfigurationReader;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -15,6 +16,7 @@ public class LoginPage extends AbstractBasePage {
 
 
     public void login() {
+        BrowserUtilities.wait(2);
         String usernameValue = ConfigurationReader.getProperty("username");
         String passwordValue = ConfigurationReader.getProperty("password");
         userName.sendKeys(usernameValue);
@@ -22,6 +24,7 @@ public class LoginPage extends AbstractBasePage {
     }
 
     public void login(String usernameValue, String passwordValue) {
+        BrowserUtilities.wait(2);
         userName.sendKeys(usernameValue);
         password.sendKeys(passwordValue, Keys.ENTER);
     }
