8af536645bd5c2c37eae8f156f9d2cc2666e04b9
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-05 16:31:58 -0400

    page classes

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -27,6 +27,10 @@ public abstract class AbstractBasePage {
         PageFactory.initElements(driver, this);
     }
 
+    /**
+     * Specify component name as a parameter, like: View all products or Orders
+     * @param component
+     */
     public void navigateTo(String component) {
         String locator = "//a[text()='" + component + "']";
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
